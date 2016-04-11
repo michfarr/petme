aec494a29b33c0846987e680134dbd249a09a63d
Carla Babadopulos <carlababa@Carlas-MacBook-Pro.local>
2016-04-11 14:57:44 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pets = Pet.create({ name: "Bob", image: "" , type: "dog", price: 30.00, rating: 4.5, description: "I'm a very nice dog!" })
+pets = Pet.create({ name: "Marley", image: "" , type: "dog", price: 35.00, rating: 5.0, description: "I'm lazy dog!" })
+pets = Pet.create({ name: "Spot", image: "" , type: "dog", price: 40.00, rating: 4.0, description: "I'm a fast dog!" })
+pets = Pet.create({ name: "Spark", image: "" , type: "dog", price: 25.00, rating: 4.5, description: "I'm a happy dog!" })
+pets = Pet.create({ name: "Misty", image: "" , type: "cat", price: 15.00, rating: 2.5, description: "I'm a very independent cat!" })
+pets = Pet.create({ name: "Spok", image: "" , type: "cat", price: 18.00, rating: 4.0, description: "I'm not emotional." })
+pets = Pet.create({ name: "Jerry", image: "" , type: "guinea pig", price: 8.00, rating: 3.0, description: "I like vegetables!" })
+pets = Pet.create({ name: "Jumpy", image: "" , type: "rabbit", price: 10.00, rating: 3.0, description: "I love to jump!" })
