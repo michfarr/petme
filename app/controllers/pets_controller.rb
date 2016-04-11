e2268b136260dca0f74c3b246b5abf4c9e9c13ed
durw4rd <misa.fasa@gmail.com>
2016-04-11 16:06:04 +0200

    seed DB, change model value 'type' to 'category'

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -69,6 +69,6 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.require(:pet).permit(:name, :image, :type, :price, :rating, :description)
+      params.require(:pet).permit(:name, :image, :category, :price, :rating, :description)
     end
 end
--- app/views/pets/index.json.jbuilder
+++ app/views/pets/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@pets) do |pet|
-  json.extract! pet, :id, :name, :image, :type, :price, :rating, :description
+  json.extract! pet, :id, :name, :image, :category, :price, :rating, :description
   json.url pet_url(pet, format: :json)
 end
