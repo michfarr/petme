5abe12fbdd1f3e7bb7f6fe9684ed2aaa3110d4f0
michfarr <etnunc@protonmail.com>
2016-04-13 17:08:19 +0200

    Validate input for User and Pet models

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,5 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, :image, :category, :price, :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :pets
+
+  validates :name, :email, presence: true
 end
