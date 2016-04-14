1fdeba4c7ca0b57d50cca32aae63402cd80f24a5
durw4rd <misa.fasa@gmail.com>
2016-04-14 16:42:12 +0200

    change layout of form, update seed file

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -44,15 +44,14 @@ class PetsController < ApplicationController
   def create
     @pet = Pet.new( pet_params, user: current_user )
 
-    respond_to do |format|
-      if @pet.save
-        format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
-        format.json { render :show, status: :created, location: @pet }
-      else
-        format.html { render :new }
-        format.json { render json: @pet.errors, status: :unprocessable_entity }
-      end
+    if @pet.save
+      redirect_to pets_path
+    else
+      render new_pet_path
     end
+
+
+    
   end
 
   # PATCH/PUT /pets/1
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,5 +2,5 @@ class Pet < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :categories
 
-  validates :name, :image, :category, :price, :description, presence: true
+  validates :name, :image, :price, :description, presence: true
 end
